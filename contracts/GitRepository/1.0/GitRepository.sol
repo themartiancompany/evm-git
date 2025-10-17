7ba780829327741f9fb19246e383fdf69869b28e
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2025-10-17 15:05:27 +0000

    hurr

--- contracts/GitRepository/1.0/GitRepository.sol
+++ contracts/GitRepository/1.0/GitRepository.sol
@@ -28,7 +28,6 @@ pragma solidity >=0.7.0 <0.9.0;
 contract GitRepository {
 
   address public immutable deployer = 0xea02F564664A477286B93712829180be4764fAe2;
-  string public hijess = "mm";
 
   mapping(
     address => mapping (
